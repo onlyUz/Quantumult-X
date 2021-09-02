341b5a96b67098f1ef6908ca6dd2711df0cd2c64
onlyUz <63804778+onlyUz@users.noreply.github.com>
2021-09-03 02:38:04 +0800

    Delete 115.js

--- js/115.js
+++
@@ -1,2 +0,0 @@
-http-response ^http:\/\/115\.com\/lx.*$ script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/115lx.js, requires-body=true
-http-response ^https?:\/\/webapi\.115\.com\/user\/check_sign.*$ script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/115lx.js, requires-body=true
